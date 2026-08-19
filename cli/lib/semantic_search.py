51afcd15e6bc9bf12843b33bb1e6edebea9040f4
lomoser <loganmoser10@protonmail.com>
2026-08-19 08:16:37 -0500

    Complete semantic search cli functionality

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,10 +1,109 @@
 from sentence_transformers import SentenceTransformer
+import numpy as np
+import os
+import json
+from .search_utils import(
+    MOVIE_EMBEDDINGS_PATH,
+    DATA_PATH,
+)
 
 class SemanticSearch:
 
     def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
         self.model = SentenceTransformer(model_name)
+        self.embeddings = None
+        self.documents = None
+        self.document_map = dict()
 
+    def generate_embedding(self, text: str) -> list[str]:
+        if len(text.strip()) == 0:
+            raise ValueError("No text to embed.")
+
+        embedding = self.model.encode([text])[0]
+
+        return embedding
+
+    def build_embeddings(self, documents: list[dict[int: str]]) -> list[str]:
+        self.documents = documents
+        for i, doc in enumerate(documents):
+            self.document_map[i] = doc
+        movie_strings = [f"{doc['title']}: {doc['description']}" for doc in documents]
+        self.embeddings = self.model.encode(movie_strings, show_progress_bar=True)
+        np.save(MOVIE_EMBEDDINGS_PATH, self.embeddings)
+
+        return self.embeddings
+
+    def load_or_build_embeddings(self, documents: list[dict[int: str]]):
+        self.documents = documents
+        for i, doc in enumerate(documents):
+            self.document_map[i] = doc
+        if os.path.isfile(MOVIE_EMBEDDINGS_PATH):
+            self.embeddings = np.load(MOVIE_EMBEDDINGS_PATH)
+            if len(self.embeddings) == len(documents):
+                return self.embeddings
+        else:
+            return self.build_embeddings(documents)
+
+    def search(self, query:str, limit: int):
+        if self.embeddings is None:
+            raise ValueError("No embeddings loaded. Call `load_or_build_embeddings` first")
+        query_embedding = self.generate_embedding(query)
+        scores = []
+        for embedding, i in zip(self.embeddings, self.document_map):
+            cos_sim = cosine_similarity(query_embedding, embedding)
+            scores.append((cos_sim, i)) # Use document map id to get title and description of movie
+
+        sorted_scores = sorted(scores, key = lambda x: x[0], reverse=True)[:limit]
+
+        results = [{"score": score, 
+                    "title": self.document_map[i]['title'], 
+                    "description": self.document_map[i]['description']} for score, i in sorted_scores]
+
+        return results
+
+
+def search_command(query: str, limit: int):
+    semantic_instance = SemanticSearch()
+    with open(DATA_PATH, 'r') as f:
+        documents = json.load(f)['movies']
+    semantic_instance.load_or_build_embeddings(documents)
+    results = semantic_instance.search(query, limit)
+    for i, result in enumerate(results):
+        print(f"{i+1}. {result['title']} (score: {result['score']}){result['description'][:100]}...")
+
+def cosine_similarity(vec1: np.ndarray, vec2: np.ndarray) -> float:
+    dot_product = np.dot(vec1, vec2)
+    norm1 = np.linalg.norm(vec1)
+    norm2 = np.linalg.norm(vec2)
+
+    if norm1 == 0 or norm2 == 0:
+        return 0.0
+
+    return dot_product / (norm1 * norm2)
+
+def verify_embeddings():
+    semantic_instance = SemanticSearch()
+    with open(DATA_PATH, 'r') as f:
+        documents = json.load(f)['movies']
+    semantic_instance.load_or_build_embeddings(documents)
+    print(f"Numer of docs: {len(semantic_instance.documents)}")
+    print(
+        f"Embeddings shape: {semantic_instance.embeddings.shape[0]} vectors in {semantic_instance.embeddings.shape[1]} dimensions"
+    )
+
+def embed_query(query: str) -> None:
+    semantic_instance = SemanticSearch()
+    embedding = semantic_instance.generate_embedding(query)
+    print(f"Query: {query}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Shape: {embedding.shape}")
+
+def embed_text(text: str) -> None:
+    semantic_instance = SemanticSearch()
+    embedding = semantic_instance.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
 
 def verify_model() -> None:
     semantic_instance = SemanticSearch()
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,5 +1,12 @@
 import argparse
-from lib.semantic_search import verify_model
+from lib.semantic_search import(
+    verify_model,
+    verify_embeddings,
+    embed_text,
+    embed_query,
+    search_command
+)
+    
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -7,11 +14,36 @@ def main() -> None:
     verify_parser = subparsers.add_parser(
         "verify", help="Verify semantic model has loaded and see max sequence length"
     )
+    embed_parser = subparsers.add_parser(
+        "embed_text", help="Create word embeddings for semantic search"
+    )
+    embed_parser.add_argument("text", type=str, help="Text to embed")
+    
+    verify_embeddings_parser = subparsers.add_parser(
+        "verify_embeddings", help="Verify document embeddings using semantic model"
+    )
+    embed_query_parser = subparsers.add_parser(
+        "embed_query", help="Embed a query so we can compare it to our document embeddings"
+    )
+    embed_query_parser.add_argument("query", type=str, help="User query to embed")
+
+    search_parser = subparsers.add_parser("search", help="Use semantic search to find similar movies")
+    search_parser.add_argument("query", type=str, help="User query to search")
+    search_parser.add_argument("--limit", nargs="?", default=5)
+
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+        case "verify_embeddings":
+            verify_embeddings()
+        case "embed_text":
+            embed_text(args.text)
+        case "embed_query":
+            embed_query(args.query)
+        case "search":
+            search_command(args.query, int(args.limit))
         case _:
             parser.print_help()
 
