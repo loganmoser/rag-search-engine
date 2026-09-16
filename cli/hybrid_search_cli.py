f68076be3b7249fade00c56e48426c77282b767b
Logan Moser <loganmoser10@protonmail.com>
2026-09-16 07:22:14 -0500

    Add basic hybrid search by combining keyword/semantic search with min/max normalized scores

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -1,13 +1,25 @@
 import argparse
 
+from lib.hybrid_search import (
+    normalize_scores
+)
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Hybrid Search CLI")
-    parser.add_subparsers(dest="command", help="Available commands")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    normalize_parser = subparsers.add_parser("normalize", help="Combine BM25 and Semanntic search by min/max normlaizing results")
+    normalize_parser.add_argument("scores", nargs="*", type=float, help="List of scores to normalize")
 
     args = parser.parse_args()
 
     match args.command:
+        case "normalize":
+            scores = normalize_scores(args.scores)
+            for score in scores:
+                print(f"* {score:.4f}")
+
         case _:
             parser.print_help()
 
