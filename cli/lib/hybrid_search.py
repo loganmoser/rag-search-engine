1ea728df11d780bb5b6ce952bbbf5a6eb215ea24
lomoser <loganmoser10@protonmail.com>
2026-09-23 20:04:18 -0500

    Add basic LLM functionality. Next step is begin working on re-ranking

--- cli/lib/hybrid_search.py
+++ cli/lib/hybrid_search.py
@@ -185,7 +185,39 @@ def rrf_search(query: str, k: int = 60, limit: int = 5, enhance: str = None):
             results = hybrid_search.rrf_search(enhanced_query, k, limit)
             for i, result in enumerate(results, 1):
                 print(f"{i}.  {result['doc']['title']}\n  RRF Score: {result['rrf_score']}\n  BM25 Rank: {result['bm25_rank']}, Semantic Rank: {result['semantic_rank']}\n  {result['doc']['document'][:50]}")
+        case "expand":
+                messages = [
+                {
+                    "role": "user",
+                    "content": f"""Rewrite the user-provided movie search query below to be more specific and searchable.
 
+                        Consider:
+                        - Common movie knowledge (famous actors, popular films)
+                        - Genre conventions (horror = scary, animation = cartoon)
+                        - Keep the rewritten query concise (under 10 words)
+                        - It should be a Google-style search query, specific enough to yield relevant results
+                        - Don't use boolean logic
+
+                        Examples:
+                        - "that bear movie where leo gets attacked" -> "The Revenant Leonardo DiCaprio bear attack"
+                        - "movie about bear in london with marmalade" -> "Paddington London marmalade"
+                        - "scary movie with bear from few years ago" -> "bear horror movie 2015-2020"
+
+                        If you cannot improve the query, output the original unchanged.
+                        Output only the rewritten query text, nothing else.
+
+                        User query: "{query}"
+                        """               }
+                ]
+                response = client.chat.completions.create(
+                    model='openrouter/free',
+                    messages=messages
+                )
+                enhanced_query = response.choices[0].message.content
+                print(f"Enhanced query ({enhance}: '{query}' -> '{enhanced_query}')\n")
+                results = hybrid_search.rrf_search(enhanced_query, k, limit)
+                for i, result in enumerate(results, 1):
+                    print(f"{i}.  {result['doc']['title']}\n  RRF Score: {result['rrf_score']}\n  BM25 Rank: {result['bm25_rank']}, Semantic Rank: {result['semantic_rank']}\n  {result['doc']['document'][:50]}")
         case _:
             results = hybrid_search.rrf_search(query, k, limit)
             for i, result in enumerate(results, 1):
